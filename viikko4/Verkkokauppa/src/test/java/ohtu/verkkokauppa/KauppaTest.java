ff556fc31c26d20f60339524643924b74e3e0233
Elina <kuukelo@gmail.com>
2021-04-16 20:03:35 +0300

    tehtävä 3 tehty

--- viikko4/Verkkokauppa/src/test/java/ohtu/verkkokauppa/KauppaTest.java
+++ viikko4/Verkkokauppa/src/test/java/ohtu/verkkokauppa/KauppaTest.java
@@ -19,25 +19,21 @@ public class KauppaTest {
     Pankki pankki;
     Viitegeneraattori viite;
     Varasto varasto;
+    Kauppa k;
 
     @Before
     public void setUp() {
         pankki = mock(Pankki.class);
         viite = mock(Viitegeneraattori.class);
         varasto = mock(Varasto.class);
-        // ...
+        k = new Kauppa(varasto, pankki, viite);
     }
 
     @Test
     public void ostoksenPaaytyttyaPankinMetodiaTilisiirtoKutsutaan() {
-        // m‰‰ritell‰‰n ett‰ tuote numero 1 on maito jonka hinta on 5 ja saldo 10
         when(varasto.saldo(1)).thenReturn(10); 
         when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
 
-        // sitten testattava kauppa 
-        Kauppa k = new Kauppa(varasto, pankki, viite);              
-
-        // tehd‰‰n ostokset
         k.aloitaAsiointi();
         k.lisaaKoriin(1);     // ostetaan tuotetta numero 1 eli maitoa
         k.tilimaksu("pekka", "12345");
@@ -48,17 +44,10 @@ public class KauppaTest {
     }
     @Test
     public void ostoksenPaaytyttyaPankinMetodiaTilisiirtoKutsutaanOikein() {
-        Pankki pankki = mock(Pankki.class);
-
-        Viitegeneraattori viite = mock(Viitegeneraattori.class);
         when(viite.uusi()).thenReturn(42);
-
-        Varasto varasto = mock(Varasto.class);
         when(varasto.saldo(1)).thenReturn(10); 
         when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
  
-        Kauppa k = new Kauppa(varasto, pankki, viite);              
-
         k.aloitaAsiointi();
         k.lisaaKoriin(1);
         k.tilimaksu("pekka", "12345");
@@ -67,19 +56,12 @@ public class KauppaTest {
     }
     @Test
     public void ostoksiaKaksiEriPankinMetodiaTilisiirtoKutsutaanOikein() {
-        Pankki pankki = mock(Pankki.class);
-
-        Viitegeneraattori viite = mock(Viitegeneraattori.class);
         when(viite.uusi()).thenReturn(42);
-
-        Varasto varasto = mock(Varasto.class);
         when(varasto.saldo(1)).thenReturn(10); 
         when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
         when(varasto.saldo(2)).thenReturn(10); 
         when(varasto.haeTuote(2)).thenReturn(new Tuote(2, "leip‰", 10));
  
-        Kauppa k = new Kauppa(varasto, pankki, viite);              
-
         k.aloitaAsiointi();
         k.lisaaKoriin(1);
         k.lisaaKoriin(2);
@@ -89,17 +71,10 @@ public class KauppaTest {
     }
     @Test
     public void ostoksiaKaksiSamaaPankinMetodiaTilisiirtoKutsutaanOikein() {
-        Pankki pankki = mock(Pankki.class);
-
-        Viitegeneraattori viite = mock(Viitegeneraattori.class);
         when(viite.uusi()).thenReturn(42);
-
-        Varasto varasto = mock(Varasto.class);
         when(varasto.saldo(1)).thenReturn(10); 
         when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
  
-        Kauppa k = new Kauppa(varasto, pankki, viite);              
-
         k.aloitaAsiointi();
         k.lisaaKoriin(1);
         k.lisaaKoriin(1);
@@ -109,18 +84,11 @@ public class KauppaTest {
     }
     @Test
     public void ostoksiaKaksiSaldiEiRiitaPankinMetodiaTilisiirtoKutsutaanOikein() {
-        Pankki pankki = mock(Pankki.class);
-
-        Viitegeneraattori viite = mock(Viitegeneraattori.class);
         when(viite.uusi()).thenReturn(42);
-
-        Varasto varasto = mock(Varasto.class);
         when(varasto.saldo(1)).thenReturn(10); 
         when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
         when(varasto.saldo(2)).thenReturn(0); 
-        when(varasto.haeTuote(2)).thenReturn(new Tuote(2, "leip‰", 10));
- 
-        Kauppa k = new Kauppa(varasto, pankki, viite);              
+        when(varasto.haeTuote(2)).thenReturn(new Tuote(2, "leip‰", 10));            
 
         k.aloitaAsiointi();
         k.lisaaKoriin(1);
